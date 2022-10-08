780892d0e108fe7bdfc77c8ac88fa2271b9983e0
ratchanon buachum <ratchanonmcnon2015@gmail.com>
2022-10-08 08:40:35 +0700

    Update main_window

--- gui/main_window/main.py
+++ gui/main_window/main.py
@@ -1,14 +1,32 @@
 import tkinter as tk
+from tkinter import messagebox
 import customtkinter
+# from ...main import confirmExit
+import sys
 
 
 def mainWindow():
-    MainWindow()
+    root = MainWindow()
+    root.start()
 
 
-class MainWindow(customtkinter.CTkToplevel):
+class MainWindow(customtkinter.CTk):
+
+    customtkinter.set_appearance_mode("dark")
 
     def __init__(self):
         super().__init__()
+
         self.title("Main")
-        self.geometry("500x400")
+        self.geometry('1280x720')
+
+        # ============ Exit ============
+        self.bind("<Escape>", self.confirmExit)
+        self.protocol("WM_DELETE_WINDOW", self.confirmExit)
+
+    def start(self):
+        self.mainloop()
+
+    def confirmExit(self, event=0):
+        if messagebox.askokcancel('Quit', 'Are you sure you want to exit?'):
+            self.quit()
